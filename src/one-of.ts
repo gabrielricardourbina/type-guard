bd9377fa3b153b4978f11f12ecf80956bda30a69
Gabriel Ricardo Urbina Royuela <gabrielurbina13@gmail.com>
2021-04-23 23:16:30 +0200

    recursive mode for OneOf

--- src/one-of.ts
+++ src/one-of.ts
@@ -1,14 +1,22 @@
-type Guard<T> = (v: unknown) => v is T
+import type { Guard } from "./types";
+import RecursiveError from "../src/recursive-error";
 
-function OneOf<G extends [Guard<any>, ...Guard<any>[]] >(guards: G): Guard<G extends Array<Guard<infer P>> ? P: never>;
-function OneOf<T>(guards: Array<T extends any ? Guard<T>: never>): Guard<T>;
-function OneOf<T>(guards: Array<T extends any ? Guard<T>: never>) {
-	return (value: unknown): value is T => {
-		return guards.some((guard) => guard(value));
-	};
+type TypeOfGuards<G extends Guard<any>[]> = G extends Guard<infer P>[]
+  ? P
+  : never;
+
+const OneOf = <T, G extends Guard<T>[] = Guard<T>[]>(
+  guards: G | ((self: Guard<T>) => G)
+): Guard<TypeOfGuards<G>> => {
+  const isOneOf = (value: unknown): value is TypeOfGuards<G> => {
+    return generatedGuards.some((guard) => guard(value));
+  };
+
+  const generatedGuards = RecursiveError.assert((forbidCall) =>
+    typeof guards === "function" ? guards(forbidCall(isOneOf)) : guards
+  );
+
+  return isOneOf;
 };
 
 export default OneOf;
-
-
-
--- test/one-of.test.ts
+++ test/one-of.test.ts
@@ -1,18 +1,52 @@
+import type { Guard } from "../src/types";
+import { expectType } from "tsd";
+import { expect } from "chai";
+import { testEach } from "./tools";
+
+import RecursiveError from "../src/recursive-error";
 import OneOf from "../src/one-of";
 import isString from "../src/is-string";
 import isNumber from "../src/is-number";
-import { expectType } from "tsd";
-import { testEach } from "./tools";
+import RecordOf from "../src/record-of";
+import isNull from "../src/is-null";
+
+
+it("OneOf: throws explicit error when trying to call the 'self' guard in recursive mode", () => {
+  expect(() =>
+    OneOf((self) => {
+      self(undefined);
+      return [self, isNull];
+    })
+  ).to.throw(RecursiveError);
+});
 
 describe("Grade: string | number", () => {
   const isGrade = OneOf([isString, isNumber]);
-  expectType<(a: unknown) => a is string | number>(isGrade);
+  expectType<Guard<string | number>>(isGrade);
 
   testEach(isGrade, "string | number", [
     [true, 10],
     [true, "A+"],
     [false, { value: 10 }],
     [false, []],
+    [false, [10, true]],
+    [false, null],
+    [false, true],
+  ]);
+});
+
+describe("Grades: string | number | Record<string, Grades>", () => {
+  type Grades = string | number | { [k: string]: Grades };
+  const isGrades = OneOf<Grades>((g) => [isString, isNumber, RecordOf(g)]);
+  expectType<Guard<Grades>>(isGrades);
+
+  testEach(isGrades, "string | number | Record<string, Grades>", [
+    [true, 10],
+    [true, "A+"],
+    [true, { math: 10, english: { writing: "A" } }],
+    [false, { math: 10, english: ["A", "B"] }],
+    [false, ["A"]],
+    [false, [10]],
     [false, [10, true]],
     [false, null],
     [false, true],
