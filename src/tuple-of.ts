185cbee15ea1a961c99ac572a99c8395324fc329
Gabriel Ricardo Urbina Royuela <gabrielurbina13@gmail.com>
2021-04-23 23:16:57 +0200

    recursive mode for TupleOf

--- src/tuple-of.ts
+++ src/tuple-of.ts
@@ -1,12 +1,26 @@
+import type { Guard } from "./types";
 import isArray from "./is-array";
+import RecursiveError from "./recursive-error";
 
-type Guard<T> = (v: unknown) => v is T
+type TypeOfGuards<G extends { [K in keyof G]: Guard<any> }> = {
+  [K in keyof G]: G[K] extends Guard<infer P> ? P : never;
+};
 
-const TupleOf = <T extends [any, ...any[]]>(guards: { [K in keyof T]: Guard<T[K]> }) => {
-	return (tuple: unknown): tuple is T => {
-		return isArray(tuple) && guards.every((guard, i) => guard(tuple[i]));
-	};
+const TupleOf = <
+  T extends [any, ...any[]],
+  G extends { [K in keyof T]: Guard<T[K]> } = { [K in keyof T]: Guard<T[K]> }
+>(
+  guards: G | ((self: Guard<TypeOfGuards<G>>) => G)
+): Guard<TypeOfGuards<G>> => {
+  const isTupleOf = (tuple: unknown): tuple is TypeOfGuards<G> => {
+    return (
+      isArray(tuple) && generatedGuards.every((guard, i) => guard(tuple[i]))
+    );
+  };
+  const generatedGuards = RecursiveError.assert((forbidCall) =>
+    typeof guards === "function" ? guards(forbidCall(isTupleOf)) : guards
+  );
+  return isTupleOf;
 };
 
 export default TupleOf;
-
--- test/tuple-of.test.ts
+++ test/tuple-of.test.ts
@@ -1,23 +1,64 @@
+import type { Guard } from "../src/types";
+import { expectType } from "tsd";
+import { expect } from "chai";
+import { testEach } from "./tools";
+
+import RecursiveError from "../src/recursive-error";
 import TupleOf from "../src/tuple-of";
 import isString from "../src/is-string";
 import isNumber from "../src/is-number";
-import { expectType } from "tsd";
-import { testEach } from "./tools";
+import isNull from "../src/is-null";
+import OneOf from "../src/one-of";
 
-describe('Person: [string, number] ', () => {
-	const isPersonTuple = TupleOf([isString, isNumber]);
-	expectType<(a: unknown) => a is [string, number]>(isPersonTuple);
-
-	testEach(isPersonTuple,"[string, number]", [[true, ["Jane Doe", 24]],
-	[true, ["Jane Doe", 23, true]],
-	[false, { brand: "Toyota", engine: 1.8 }],
-	[false, ["Jane Doe", "23"]],
-	[false, []],
-	[false, ["10", true]],
-	[false, [10, true]],
-	[false, null],
-	[false, 10],
-	[false, "10"],
-	[false, true],])
+it("TupleOf: throws explicit error when trying to call the 'self' guard in recursive mode", () => {
+  expect(() =>
+    TupleOf((self) => {
+      self(undefined);
+      return [self, isNull];
+    })
+  ).to.throw(RecursiveError);
 });
 
+describe("Person: [string, number] ", () => {
+  const isPersonTuple = TupleOf([isString, isNumber]);
+  expectType<Guard<[string, number]>>(isPersonTuple);
+
+  testEach(isPersonTuple, "[string, number]", [
+    [true, ["Jane Doe", 24]],
+    [true, ["Jane Doe", 23, true]],
+    [false, { brand: "Toyota", engine: 1.8 }],
+    [false, ["Jane Doe", "23"]],
+    [false, []],
+    [false, ["10", true]],
+    [false, [10, true]],
+    [false, null],
+    [false, 10],
+    [false, "10"],
+    [false, true],
+  ]);
+});
+
+describe("Person: [string, number, Person | null] ", () => {
+  type Person = [string, number, Person | null];
+  const isPersonTuple = TupleOf<Person>((g) => [
+    isString,
+    isNumber,
+    OneOf([g, isNull]),
+  ]);
+  expectType<Guard<Person>>(isPersonTuple);
+
+  testEach(isPersonTuple, "[string, number, Person | null]", [
+    [true, ["Jane Doe", 24, null]],
+    [true, ["Jane Doe", 23, ["Peter Doe", 54, null]]],
+    [false, ["Jane Doe", 23, true]],
+    [false, { brand: "Toyota", engine: 1.8 }],
+    [false, ["Jane Doe", "23"]],
+    [false, []],
+    [false, ["10", true]],
+    [false, [10, true]],
+    [false, null],
+    [false, 10],
+    [false, "10"],
+    [false, true],
+  ]);
+});
