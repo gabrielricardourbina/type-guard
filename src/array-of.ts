d2e2afc3af78cdad780c0ccaf15d89fff879c5b2
Gabriel Ricardo Urbina Royuela <gabrielurbina13@gmail.com>
2021-04-27 09:26:13 +0200

    recursive mode for ArrayOf

--- src/array-of.ts
+++ src/array-of.ts
@@ -1,5 +1,6 @@
 import type { Guard } from "./types";
 import isArray from "./is-array";
+import RecursiveError from "./recursive-error";
 
 type TypeOfGuards<G extends Guard<any>[]> = G extends Guard<infer P>[]
   ? P[]
@@ -9,13 +10,20 @@ const ArrayOf = <
   T extends any[],
   G extends { [K in keyof T]: Guard<T[K]> } = { [K in keyof T]: Guard<T[K]> }
 >(
-  guards: G
+  guards: G | ((self: Guard<TypeOfGuards<G>>) => G)
 ): Guard<TypeOfGuards<G>> => {
-  return (arr: unknown): arr is TypeOfGuards<G> => {
+  const isArrayOf = (arr: unknown): arr is TypeOfGuards<G> => {
     return (
-      isArray(arr) && arr.every((value) => guards.some((guard) => guard(value)))
+      isArray(arr) &&
+      arr.every((value) => generatedGuards.some((guard) => guard(value)))
     );
   };
+
+  const generatedGuards = RecursiveError.assert((forbidCall) =>
+    typeof guards === "function" ? guards(forbidCall(isArrayOf)) : guards
+  );
+
+  return isArrayOf;
 };
 
 export default ArrayOf;
--- test/array-of.test.ts
+++ test/array-of.test.ts
@@ -1,10 +1,22 @@
 import type { Guard } from "../src/types";
 import { expectType } from "tsd";
+import { expect } from "chai";
 import { testEach } from "./tools";
 
+import RecursiveError from "../src/recursive-error";
 import ArrayOf from "../src/array-of";
 import isString from "../src/is-string";
 import isNumber from "../src/is-number";
+import isNull from "../src/is-null";
+
+it("ArrayOf: throws explicit error when trying to call the 'self' guard in recursive mode", () => {
+  expect(() =>
+    ArrayOf((self) => {
+      self(undefined);
+      return [self, isNull];
+    })
+  ).to.throw(RecursiveError);
+});
 
 describe("Names: string[]", () => {
   const guard = ArrayOf([isString]);
@@ -26,7 +38,7 @@ describe("Names: string[]", () => {
 
 describe("NamesOrId: (string| number)[]", () => {
   const guard = ArrayOf([isString, isNumber]);
-  expectType<Guard<(string|number)[]>>(guard);
+  expectType<Guard<(string | number)[]>>(guard);
 
   testEach(guard, "(string| number)[]", [
     [true, ["Victor", "Elena", "Maria"]],
@@ -42,3 +54,22 @@ describe("NamesOrId: (string| number)[]", () => {
   ]);
 });
 
+describe("FullNames: (string | FullNames)[]", () => {
+  type FullNames = (string | FullNames)[];
+  const guard = ArrayOf<FullNames>((g) => [isString, g]);
+  expectType<Guard<FullNames>>(guard);
+
+  testEach(guard, "(string | FullNames)[]", [
+    [true, ["Victor", "Elena", "Maria"]],
+    [true, []],
+    [true, ["Victor", ["Elena", "Maria"]]],
+    [false, ["Victor", 12, "Maria"]],
+    [false, { brand: "Toyota", engine: 1.8 }],
+    [false, ["10", true]],
+    [false, [10, true]],
+    [false, null],
+    [false, 10],
+    [false, "10"],
+    [false, true],
+  ]);
+});
