7e0aa595a3f5ff7ebc8625328c83f01c35296249
Gabriel Ricardo Urbina Royuela <gabrielurbina13@gmail.com>
2021-05-01 21:57:38 +0200

    recursive mode for RecordOf

--- src/record-of.ts
+++ src/record-of.ts
@@ -1,22 +1,28 @@
 import type { Guard } from "./types";
 import isObject from "./is-object";
+import RecursiveError from "./recursive-error";
 
 type TypeOfGuards<G extends Guard<any>[]> = G extends Guard<infer P>[]
-  ? P
+  ? { [k: string]: P }
   : never;
 
 const RecordOf = <
   T extends { [k: string]: any },
   G extends Guard<T[string]>[] = Guard<T[string]>[]
 >(
-  guards: G
-): Guard<{ [k: string]: TypeOfGuards<G> }> => {
-  return (rec: unknown): rec is { [k: string]: TypeOfGuards<G> } => {
+  guards: G | ((self: Guard<TypeOfGuards<G>>) => G)
+): Guard<TypeOfGuards<G>> => {
+  const isRecordOf = (rec: unknown): rec is TypeOfGuards<G> => {
     return (
       isObject(rec) &&
-      Object.values(rec).every((v) => guards.some((guard) => guard(v)))
+      Object.values(rec).every((v) => generatedGuards.some((guard) => guard(v)))
     );
   };
+
+  const generatedGuards = RecursiveError.assert((forbidCall) =>
+    typeof guards === "function" ? guards(forbidCall(isRecordOf)) : guards
+  );
+  return isRecordOf;
 };
 
 export default RecordOf;
--- test/record-of.test.ts
+++ test/record-of.test.ts
@@ -1,9 +1,21 @@
 import type { Guard } from "../src/types";
 import { expectType } from "tsd";
+import { expect } from "chai";
 import { testEach } from "./tools";
 
+import RecursiveError from "../src/recursive-error";
 import RecordOf from "../src/record-of";
 import isNumber from "../src/is-number";
+import isNull from "../src/is-null";
+
+it("RecordOf: throws explicit error when trying to call the 'self' guard in recursive mode", () => {
+  expect(() =>
+    RecordOf((self) => {
+      self(undefined);
+      return [self, isNull];
+    })
+  ).to.throw(RecursiveError);
+});
 
 describe("Grades: { [name: string]: number }", () => {
   const isGrades = RecordOf([isNumber]);
@@ -11,6 +23,29 @@ describe("Grades: { [name: string]: number }", () => {
 
   testEach(isGrades, "{ [name: string]: number }", [
     [true, { "Jane Doe": 9, "Jon Snow": 10 }],
+    [true, {}],
+    [false, []],
+    [false, [1, 2, 3, 4]],
+    [false, { "Jane Doe": 9, "Jon Snow": "A+" }],
+    [false, { brand: "Toyota", engine: 1.8 }],
+    [false, ["Jane Doe", "23"]],
+    [false, ["10", true]],
+    [false, [10, true]],
+    [false, null],
+    [false, 10],
+    [false, "10"],
+    [false, true],
+  ]);
+});
+
+describe("Grades: { [name: string]: number | Grades }", () => {
+  type Grades = { [name: string]: number | Grades };
+  const isGrades = RecordOf<Grades>((self) => [isNumber, self]);
+  expectType<Guard<Grades>>(isGrades);
+
+  testEach(isGrades, "{ [name: string]: number | Grades }", [
+    [true, { "Jane Doe": 9, "Jon Snow": 10 }],
+    [true, { "Jane Doe": 9, "Jon Snow": 10, "Jon Ripper": { Carl: 23 } }],
     [true, {}],
     [false, []],
     [false, [1, 2, 3, 4]],
