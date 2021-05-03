91929eb0ed2333701dd695bba78fc7d668217126
Gabriel Ricardo Urbina Royuela <gabrielurbina13@gmail.com>
2021-05-03 14:51:59 +0200

    Use the same README.md for npm

--- prerelease.js
+++ prerelease.js
@@ -7,18 +7,7 @@ const fs = require("fs").promises;
     scripts: undefined,
   };
   const packageString = await fs.readFile("package.json", "utf8");
-  const readmeString = await fs.readFile("README.md", "utf8");
-
-  const modulesString = await fs.readFile("modules.md", "utf8");
   const packageConfig = JSON.parse(packageString);
-
-  const resolvedDocs = `${readmeString}\n${modulesString}`
-    .replace(/\nDefined in:.*\n/gm, "")
-    .replace(/modules\.md/gm, "README.md")
-    .replace(
-      `# ${packageConfig.name} - v${packageConfig.version}`,
-      `# Exports - v${packageConfig.version}`
-    );
   const releaseConfig = Object.assign(packageConfig, configOverride);
 
   await fs.writeFile(
@@ -26,11 +15,7 @@ const fs = require("fs").promises;
     JSON.stringify(releaseConfig, null, "\t"),
     "utf8"
   );
-  await fs.writeFile(
-    "dist/README.md",
-    resolvedDocs,
-    "utf8"
-  );
 
   await fs.copyFile("LICENSE", "dist/LICENSE");
+  await fs.copyFile("README.md", "dist/README.md");
 })();
